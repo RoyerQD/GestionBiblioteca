b2114bd9301901dd534fb60899e3ef41183dcfe0
RoyerQD <royer77890@gmail.com>
2025-05-13 21:49:59 -0500

    Controlador

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
+    
+    public function create()
+    {
+        return view('books.create');
+    }
+    
+    public function store(Request $request)
+    {
+        Book::create($request->all());
+        return redirect()->route('books.index');
+    }
+    
+    public function edit(Book $book)
+    {
+        return view('books.edit', compact('book'));
+    }
+    
+    public function update(Request $request, Book $book)
+    {
+        $book->update($request->all());
+        return redirect()->route('books.index');
+    }
+    
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index');
+    }
+}
